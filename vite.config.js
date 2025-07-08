9c401791e0f5a5e985c7c39f8d66d4415a1e7727
Filipe Aguiar <filipe.aguiar@ufpe.br>
2025-07-08 09:56:36 -0300

    feat: remove vite-plugin-pwa to fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: '3DeT Victory Shield',
-        short_name: '3DeT Shield',
-        description: 'Um escudo do mestre para 3DeT Victory',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+    tailwindcss()],
 })
